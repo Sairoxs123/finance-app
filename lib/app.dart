5e65430f3213c6e2b53507e86fe019b074c844e0
Sairoxs123 <127499321+Sairoxs123@users.noreply.github.com>
2025-07-11 15:56:57 +0400

    first commit

---
+++ lib/app.dart
@@ -0,0 +1,29 @@
+import 'package:finance_app/screens/home.dart';
+import 'package:finance_app/services/auth_service.dart';
+import 'package:flutter/material.dart';
+import 'screens/landing.dart';
+import 'package:provider/provider.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Finance App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Consumer<AuthService>(
+        builder: (context, authService, _) {
+          print(authService.currentUser?.email);
+          if (authService.currentUser == null) {
+            return const LandingPage();
+          } else {
+            return const HomePage();
+          }
+        },
+      ), // Start with ChatHomePage
+    );
+  }
+}
